27a6dfc2d6a32764a743672b462c508452067ba6
pamex1 <pamdavou12@gmail.com>
2022-09-10 10:31:50 -0700

    puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
 * this program prints "programming is like building a multilingual puzzle
 * Return: 0
 */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* print the string in the put function
+*
+*Description: using the main function
+*this program prints "programming is like building multilingual puzzle
+*Return: 0
+*/
+int main(void)
+{
+	char c;
+	int i:
