718b32485cd94bf8f99e423d93b33baea3d35339
pamex1 <pamdavou12@gmail.com>
2022-09-19 19:54:41 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,14 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _putchar(char c);
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _puts(char *s);
+int _strlen(char *s);
+void _puts(char *s);
+int _putchar(char c);
 void print_rev(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 void rev_string(char *s);
-int _strlen(char *s);
+
 #endif
