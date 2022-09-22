e8c09f592bbe0593694c0ae818df5bfc4977295a
pamex1 <pamdavou12@gmail.com>
2022-09-21 23:36:51 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 100; num++)
+	{
+		putchar((num / 10) + '0');
+		putchar((num % 10) + '0');
+		if (num < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
