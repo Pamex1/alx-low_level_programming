707aff1e432999bfc9a9985be29541a711349aee
pamex1 <pamdavou12@gmail.com>
2022-09-22 13:40:55 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,7 @@
 /**
 *cap_string - function that capitalize first character of a word
 *@str: string to capitalize
-*Return: returns the capitalized string
+*Return:returns the capitalized string
 */
 char *cap_string(char *str)
 {
