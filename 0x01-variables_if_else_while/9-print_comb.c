22d3b48c6945dd57a3d62ea35650871fcd0c5eb5
pamex1 <pamdavou12@gmail.com>
2022-09-21 23:17:48 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main -  program that prints all possible combinations of single-digit
+ * separated by ,followed by a space
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n = 48;
+	int com = 44;
+
+	while (n <= 57)
+	{
+	putchar(n);
+	if (n != 57)
+	{
+		putchar(com);
+		putchar(32);
+	}
+	n += 1;
+	}
+	putchar('\n');
+	return (0);
+}
