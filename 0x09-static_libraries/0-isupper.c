253c1cf401fdb903557c18d031fed559578107c1
pamex1 <pamdavou12@gmail.com>
2022-10-03 12:13:39 -0700

    task 0

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_isupper - checks if a character is upper case or not
+*@c: character to be tested
+*Return: 1 whether it is, or otherwise
+*/
+
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+	{
+		return (1);
+	}
+
+	return (0);
+}
---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ *
+ * @s: hold each character after the count
+ *
+ * Return: Length of a string
+ */
+
+int _strlen(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0'; index++)
+		;
+	return (index);
+}
