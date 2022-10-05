ff149a1909aa479befcb8fa2aaac84c6e1b3e8e2
pamex1 <pamdavou12@gmail.com>
2022-10-05 00:10:07 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,23 +9,23 @@
  */
 int count_word(char *s)
 {
- int flag, c, w;
+int flag, c, w;
 
- flag = 0;
- w = 0;
+flag = 0;
+w = 0;
 
- for (c = 0; s[c] != '\0'; c++)
- {
- if (s[c] == ' ')
- flag = 0;
- else if (flag == 0)
- {
- flag = 1;
- w++;
- }
- }
+for (c = 0; s[c] != '\0'; c++)
+{
+if (s[c] == ' ')
+flag = 0;
+else if (flag == 0)
+{
+flag = 1;
+w++;
+}
+}
 
- return (w);
+return (w);
 }
 /**
  * **strtow - splits a string into words
@@ -36,42 +36,42 @@ int count_word(char *s)
  */
 char **strtow(char *str)
 {
- char **matrix, *tmp;
- int i, k = 0, len = 0, words, c = 0, start, end;
+char **matrix, *tmp;
+int i, k = 0, len = 0, words, c = 0, start, end;
 
- while (*(str + len))
- len++;
- words = count_word(str);
- if (words == 0)
- return (NULL);
+while (*(str + len))
+len++;
+words = count_word(str);
+if (words == 0)
+return (NULL);
 
- matrix = (char **) malloc(sizeof(char *) * (words + 1));
- if (matrix == NULL)
- return (NULL);
+matrix = (char **) malloc(sizeof(char *) * (words + 1));
+if (matrix == NULL)
+return (NULL);
 
- for (i = 0; i <= len; i++)
- {
- if (str[i] == ' ' || str[i] == '\0')
- {
- if (c)
- {
- end = i;
- tmp = (char *) malloc(sizeof(char) * (c + 1));
- if (tmp == NULL)
- return (NULL);
- while (start < end)
- *tmp++ = str[start++];
- *tmp = '\0';
- matrix[k] = tmp - c;
- k++;
- c = 0;
- }
- }
- else if (c++ == 0)
- start = i;
- }
+for (i = 0; i <= len; i++)
+{
+if (str[i] == ' ' || str[i] == '\0')
+{
+if (c)
+{
+end = i;
+tmp = (char *) malloc(sizeof(char) * (c + 1));
+if (tmp == NULL)
+return (NULL);
+while (start < end)
+*tmp++ = str[start++];
+*tmp = '\0';
+matrix[k] = tmp - c;
+k++;
+c = 0;
+}
+}
+else if (c++ == 0)
+start = i;
+}
 
- matrix[k] = NULL;
+matrix[k] = NULL;
 
- return (matrix);
+return (matrix);
 }
