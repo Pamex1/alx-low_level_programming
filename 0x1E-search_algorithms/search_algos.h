94ff75bafff009934c130050d75b59bfd8957425
Pamex1 <111292903+Pamex1@users.noreply.github.com>
2023-06-28 12:13:25 +0100

    Delete 0x1E-search_algorithms directory

--- 0x1E-search_algorithms/search_algos.h
+++
@@ -1,9 +0,0 @@
-#ifndef SEARCH_H
-#define SEARCH_H
-
-#include <stdio.h>
-
-/* Prototypes*/
-int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
-#endif
